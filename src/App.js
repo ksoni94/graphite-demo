7bd7739b0a069ceb4be3cadafb19adb01b44c00d
Karan <karan@savemyexams.co.uk>
2023-06-14 16:13:21 +0100

    update link

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://graphite.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Graphite
         </a>
       </header>
     </div>
